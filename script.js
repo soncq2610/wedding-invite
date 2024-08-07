cd024be812b3b1a46e1623cc9ebcfec91b98d2b7
Eugene.Cong <eugene.cong@avepoint.com>
2024-08-07 17:04:10 +0700

    update coundown section and some new img

---
+++ script.js
@@ -0,0 +1,25 @@
+var countDownDate = new Date("Nov 2, 2024 00:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days < 10 ? "0" + days : days;
+  document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
+  document.getElementById("minutes").innerHTML =
+    minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById("seconds").innerHTML =
+    seconds < 10 ? "0" + seconds : seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  }
+}, 1000);
